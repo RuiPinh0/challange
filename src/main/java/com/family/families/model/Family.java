4db6c76321b7f7e2ac5d84fe8dd096e83682a0ae
Rui Pinho <r.pinho@re-think.consulting>
2020-07-02 14:52:18 +0100

    Models initial setup

--- src/main/java/com/family/families/model/Family.java
+++ src/main/java/com/family/families/model/Family.java
@@ -1,5 +1,8 @@
 package com.family.families.model;
 
+import lombok.*;
+
+@Getter @Setter
 public class Family {
     String name;
     String country;
--- src/main/java/com/family/families/model/Member.java
+++ src/main/java/com/family/families/model/Member.java
@@ -1,8 +1,18 @@
 package com.family.families.model;
 
+import lombok.*;
+import java.util.Date;
+
+@Getter @Setter
 public class Member {
 
     Family family;
     String firstName;
+    String middleName;
+    Member father;
+    Member mother;
+    Member spouse;
+    Date birthDate;
+
 
 }
